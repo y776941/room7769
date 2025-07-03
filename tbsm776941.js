e886e6a10a6278653034bb05b1d3834097502a99
y776941 <y776941908@gmail.com>
2025-07-03 17:40:09 +0300

    Update tbsm776941.js

--- tbsm776941.js
+++ tbsm776941.js
@@ -220,7 +220,7 @@ $("style").last().append(`
   border: 1px solid #ecdce4;
 }
 
-#d2.ashq1{background-color:#fce4ee !important;}
+#d2.ashq1{background-color:#e4e4e4 !important;}
 
 #room.ashq1 .tablebox.d-flex.footer.fl.light {
   border-radius: 0 10px 0 10px !important;
