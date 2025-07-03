0c564cd295ff421da01e098e2993ea33709961ce
y776941 <y776941908@gmail.com>
2025-07-03 03:28:08 +0300

    Update tbsm776941.js

--- tbsm776941.js
+++ tbsm776941.js
@@ -3,7 +3,7 @@ $("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
 background-size: 220% auto;border-radius: 2px !important;border: 1px solid #0000;
 }`);
 $("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
-background-image: url(https://up6.cc/2025/07/175149167711131.png); background-size: cover;color: #666666;
+background-image: url(https://up6.cc/2025/07/175150232967091.gif); background-size: cover;color: #666666;
 }`)
 
 
