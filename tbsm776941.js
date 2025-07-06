f61389277700628381f7e4a0de8ab0d24794e15e
y776941 <y776941908@gmail.com>
2025-07-06 12:24:28 +0300

    Update tbsm776941.js

--- tbsm776941.js
+++ tbsm776941.js
@@ -328,7 +328,7 @@ height: 52px;
   border-radius: 2px !important;
   border: 1px solid #ecdce4;
   background-color: #b2b2b2!important;
-  background-image: url(https://up6.cc/2025/07/175175589642971.png);
+  background-image: url(https://up6.cc/2025/07/175179367761351.png);
   background-size: cover;
   color: #fff;
 }
