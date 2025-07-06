715eb974669d50708f0efb6962446d9b27c45ed6
y776941 <y776941908@gmail.com>
2025-07-06 08:45:53 +0300

    Update tbsm776941.js

--- tbsm776941.js
+++ tbsm776941.js
@@ -170,7 +170,7 @@ $(`<style>
 }
 .nosel.d-flex.room.r197cb46eea3x141d6685x-wt4n0m .corner.fa.fa-user.label.uc.fa-microphone.label-danger {
 letter-spacing: 1px;
--webkit-text-fill-color: #000;
+-webkit-text-fill-color: #fff;
 height: 27px;
 margin-top: 40px !important;
 margin-bottom: 0px !important;
